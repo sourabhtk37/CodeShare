ed463f2cb87f7bde8742956ec90445363ae87c24
wd002 <drdcsharma80@gmail.com>
2017-03-09 02:14:37 +0530

    Added SSL support for the webapp All HTTP traffics are permenantly redirected to HTTPS. The nginx configuration for the same can be found in config/ and the cron job setting for auto renewal of the letsencrypt ssl files is also added in config/.
    This is a fix for #15
    
    Updated templates

--- code_share/app_code_share/urls.py
+++ code_share/app_code_share/urls.py
@@ -4,5 +4,5 @@ from .views import *
 urlpatterns = [
     url(r'^$', home, name='app_home'),
     url(r'^app/(?P<hash_id>\d+)/$', view_by_hash, name='view_by_hash'),
-    url(r'^app/(?P<file_name>[\w-]+)/$', view_by_file, name='view_by_file')
+    # url(r'^app/(?P<file_name>[\w-]+)/$', view_by_file, name='view_by_file')
 ]
