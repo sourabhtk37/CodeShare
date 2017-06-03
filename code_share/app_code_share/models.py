23ae6f4f1cc1a4fece9dac63715733b8e1260b0c
T K Sourabh <sourabhtk37@gmail.com>
2017-06-03 16:24:50 +0530

    Updated document strings
    Fixes #13

--- code_share/app_code_share/models.py
+++ code_share/app_code_share/models.py
@@ -1,24 +1,30 @@
 from __future__ import unicode_literals
-
 from django.db import models
 
 
-
 class CodeShare(models.Model):
-    
-	code = models.TextField(max_length=100000, 
-                            null=True, 
+    """
+    Databse schema for handline the code snippet data
+
+    :field code: text field for code content
+    :field hash_value: unique hash ID for distinguishing code snipeets
+    :field file_name: character field for file name
+
+    """
+
+    code = models.TextField(max_length=100000,
+                            null=True,
                             blank=True,
-                            )
-	hash_value = models.SlugField(max_length=100,
-                              null=True,
-                              blank=True,
-                              unique=True)
-	file_name = models.CharField(max_length=50,
-                                null=True,
-                                blank=True)	
+                            unique=False)
 
-	def __unicode__(self):
-		return str(self.file_name)
+    hash_value = models.SlugField(max_length=100,
+                                  null=True,
+                                  blank=True,
+                                  unique=True)
 
-	 	
+    file_name = models.CharField(max_length=50,
+                                 null=True,
+                                 blank=True)
+
+    def __unicode__(self):
+        return str(self.hash_value)
