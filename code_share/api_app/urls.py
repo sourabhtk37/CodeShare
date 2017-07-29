a3264d542bd0ce909bc85ecb0d92b30d57e6eb80
Mannu gupta <abhimanyu98986@gmail.com>
2017-07-29 16:09:32 +0530

    modified code for pep-8 standard

--- code_share/api_app/urls.py
+++ code_share/api_app/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url, include
+from django.conf.urls import url
 from .views import *
 from rest_framework.urlpatterns import format_suffix_patterns as suffix
 
@@ -6,6 +6,7 @@ from rest_framework.urlpatterns import format_suffix_patterns as suffix
 urlpatterns = [
     url(r'^$', api_home, name='api_home'),
     url(r'^(?P<hash_id>\d+)/$', code_by_hash, name='return_by_hash'),
-    url(r'^(?P<file_name>[\w-]+)/$', code_by_filename, name='return_by_filename'),
+    url(r'^(?P<file_name>[\w-]+)/$',
+        code_by_filename, name='return_by_filename'),
 ]
-urlpatterns=suffix(urlpatterns)
+urlpatterns = suffix(urlpatterns)
