e85363e6e5b8edda990ec98a6c3137af7a9f1349
nishutosh <nishutosh002@gmail.com>
2017-08-26 14:04:20 +0530

    admin site view improvments

--- code_share/app_code_share/admin.py
+++ code_share/app_code_share/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 from .models import CodeShare
 
 
-admin.site.register(CodeShare)
-
+@admin.register(CodeShare)
+class CodeShareAdmin(admin.ModelAdmin):
+     exclude = ('id',)
+     list_display = ('hash_value', 'language','file_name')
